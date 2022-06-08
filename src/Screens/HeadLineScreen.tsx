c5838d72ab60757b2c08d7ba640dd10c640b115b
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2022-06-08 19:04:09 -0400

    adding a lib to parse draft-js

--- src/Screens/HeadLineScreen.tsx
+++ src/Screens/HeadLineScreen.tsx
@@ -7,6 +7,7 @@ import { RawDraftContentBlock } from 'draft-js';
 
 import { RootNavStackParamList } from '../App';
 import { AccessibleImage, IconButton } from '../Components';
+import getRNDraftJSBlocks from 'react-native-draftjs-render';
 
 type HeadLineScreenProps = StackScreenProps<RootNavStackParamList, 'Headline'>;
 
@@ -39,15 +40,9 @@ const HeadLineScreen: FC<HeadLineScreenProps> = ({ navigation, route }) => {
   }, []);
 
   const getBodyText = (blocks: Array<RawDraftContentBlock>) => {
-    return blocks.map((item) => {
-      const { text } = item;
-      return (
-        !!text && (
-          <Text key={item.key} style={styles.blockContainer}>
-            {text}
-          </Text>
-        )
-      );
+    return getRNDraftJSBlocks({
+      contentState: blocks,
+      customStyles: drafJsContentStyle,
     });
   };
 
@@ -90,7 +85,7 @@ const HeadLineScreen: FC<HeadLineScreenProps> = ({ navigation, route }) => {
             altText={storyData.primary_image?.alt_text}
           />
         </View>
-        {getBodyText(storyData.blocks.blocks)}
+        {getBodyText(storyData.blocks)}
         <View style={styles.bottomButtonSection}>
           <IconButton
             iconName="RouteIcon"
@@ -147,11 +142,71 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginBottom: 10,
   },
-  blockContainer: {
-    marginBottom: 20,
-  },
   topicText: {
-    marginBottom: 20,
+    marginBottom: 10,
     fontSize: 12,
+  },
+});
+
+const drafJsContentStyle = StyleSheet.flatten({
+  unstyled: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: 18,
+    marginBottom: 10,
+  },
+  link: {
+    color: '#2257DA',
+    fontWeight: 'bold',
+  },
+  unorderedListItemContainer: {
+    marginBottom: 15,
+    position: 'relative',
+    marginLeft: 19,
+  },
+  unorderedListItemBullet: {
+    marginRight: 5,
+    position: 'relative',
+    top: 6,
+    width: 6,
+    height: 6,
+    alignSelf: 'flex-start',
+  },
+  'unordered-list-item': {
+    fontSize: 14,
+    lineHeight: 18,
+    alignSelf: 'flex-start',
+    flex: 1,
+  },
+  orderedListContainer: {
+    marginBottom: 16,
+  },
+  orderedListItemNumber: {
+    fontSize: 14,
+    lineHeight: 32,
+    marginRight: 11,
+    alignSelf: 'flex-start',
+    color: '#c4170c',
+  },
+  'ordered-list-item': {
+    alignSelf: 'flex-start',
+    fontSize: 14,
+    lineHeight: 32,
+    flex: 1,
+  },
+  'code-block': {
+    backgroundColor: '#e2e2e2',
+  },
+  blockquote: {
+    fontWeight: 'bold',
+    color: '#333',
+    lineHeight: 33,
+    paddingTop: 24,
+    marginBottom: 10,
+    fontSize: 33,
+    letterSpacing: -2,
+  },
+  viewAfterList: {
+    marginBottom: 10,
   },
 });
